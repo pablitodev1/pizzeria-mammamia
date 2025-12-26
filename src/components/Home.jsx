e19c50aab29ce06ab141a2f9caa3c8b075279d4e
pablitodev1 <pablo.navarro.campos@gmail.com>
2025-12-26 13:47:21 -0300

    comienzo branch entrega 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ import Header from "./Header"
 const Home = () => {
   return (
     <div className={styles.home}>
-        <Header/>
-        <div className={styles.cards_container}>
-        <CardPizza imagen="https://ooni.com/cdn/shop/articles/20220211142347-margherita-9920_ba86be55-674e-4f35-8094-2067ab41a671.jpg?v=1737104576&width=1080" nombre="Pizza Margherita" ingredientes="Burrata, Albahaca, Oregano y Mozarella." precio="6.990"/>
+      <Header />
+      <div className={styles.cards_container}>
+        <CardPizza imagen="https://ooni.com/cdn/shop/articles/20220211142347-margherita-9920_ba86be55-674e-4f35-8094-2067ab41a671.jpg?v=1737104576&width=1080" nombre="Pizza Margherita" ingredientes="Burrata, Albahaca, Oregano y Mozarella." precio="6.990" />
 
-          <CardPizza imagen="https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Diavola.jpg" nombre="Pizza Diavola" ingredientes="Mozarella, Albahaca y Pepperoni picante." precio="7.490"/>
+        <CardPizza imagen="https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Diavola.jpg" nombre="Pizza Diavola" ingredientes="Mozarella, Albahaca y Pepperoni picante." precio="7.490" />
 
-            <CardPizza imagen="https://img.freepik.com/fotos-premium/pizza-cuatro-carnes-salchicha-carne-jamon-pollo-sobre-hormigon-oscuro_210632-1746.jpg" nombre="Pizza 4 Carnes" ingredientes="Mozarella, Pepperoni, Chorizo, Pollo bbq y Tocino" precio="8.990"/>
-        </div>
+        <CardPizza imagen="https://img.freepik.com/fotos-premium/pizza-cuatro-carnes-salchicha-carne-jamon-pollo-sobre-hormigon-oscuro_210632-1746.jpg" nombre="Pizza 4 Carnes" ingredientes="Mozarella, Pepperoni, Chorizo, Pollo bbq y Tocino" precio="8.990" />
+      </div>
 
     </div>
   )
